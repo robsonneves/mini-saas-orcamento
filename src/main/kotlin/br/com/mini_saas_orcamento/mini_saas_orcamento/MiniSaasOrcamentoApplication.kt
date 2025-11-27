850d88ae2b1ab98d092432ba5906d0c8b86a56af
robsonneves <robsonnevesrodrigues@yahoo.com.br>
2025-11-27 19:26:22 -0300

    Start project

---
+++ src/main/kotlin/br/com/mini_saas_orcamento/mini_saas_orcamento/MiniSaasOrcamentoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.mini_saas_orcamento.mini_saas_orcamento
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniSaasOrcamentoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniSaasOrcamentoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/mini_saas_orcamento/mini_saas_orcamento/MiniSaasOrcamentoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.mini_saas_orcamento.mini_saas_orcamento
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniSaasOrcamentoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
